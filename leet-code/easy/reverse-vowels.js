cb497c3834e32956dc402f8b80faa0dc6b217771
jltrench <jltrench@gmail.com>
2025-01-06 21:39:19 -0300

    lc: reverse vowels

---
+++ leet-code/easy/reverse-vowels.js
@@ -0,0 +1,34 @@
+// Problem: Given a string s, reverse only all the vowels in the string and return it. The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseVowels = function (s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+
+    const chars = s.split('');
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !vowels.has(chars[left])) {
+            left++;
+        }
+
+        while (left < right && !vowels.has(chars[right])) {
+            right--;
+        }
+
+        if (left < right) {
+            [chars[left], chars[right]] = [chars[right], chars[left]];
+            left++;
+            right--;
+        }
+    }
+
+    return chars.join('');
+};
+
+console.log(reverseVowels('IceCreAm')) // 'holle'
